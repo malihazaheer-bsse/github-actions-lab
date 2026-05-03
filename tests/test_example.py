d259ed4f0f9d8c7387289fc1a7f947949d38dada
malihazaheer-bsse <maliha.bsse4676@student.iiu.edu.pk>
2026-05-03 17:44:07 +0500

    testing failure

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestApp(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(2 + 3, 5)
+        self.assertEqual(2 + 3, 6)
 
 if __name__ == "__main__":
     unittest.main()
